71c6df44be1f0e4d91574781bd82fe5238435601
manloog <128306464+manloog@users.noreply.github.com>
2024-02-05 10:29:38 +0800

    Add files via upload

---
+++ main.d
@@ -0,0 +1,5 @@
+.\main.o: User\main.c
+.\main.o: .\Start\stm32f10x.h
+.\main.o: .\Start\core_cm3.h
+.\main.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\main.o: .\Start\system_stm32f10x.h
---
+++ system_stm32f10x.d
@@ -0,0 +1,5 @@
+.\system_stm32f10x.o: Start\system_stm32f10x.c
+.\system_stm32f10x.o: Start\stm32f10x.h
+.\system_stm32f10x.o: Start\core_cm3.h
+.\system_stm32f10x.o: D:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\system_stm32f10x.o: Start\system_stm32f10x.h
